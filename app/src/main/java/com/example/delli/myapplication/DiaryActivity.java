026806ae38ce68df1e63b57aaf52f6787d5473fb
Delli <ronja.kammerer@stud.uni-regensburg.de>
2018-09-03 20:55:14 +0200

    Datenbank erstellt

--- app/src/main/java/com/example/delli/myapplication/DiaryActivity.java
+++ app/src/main/java/com/example/delli/myapplication/DiaryActivity.java
@@ -3,58 +3,58 @@ package com.example.delli.myapplication;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.ArrayAdapter;
+import java.util.List;
 
 
 public class DiaryActivity extends AppCompatActivity {
+
+    public static final String LOG_TAG = DiaryActivity.class.getSimpleName();
+    private DiaryMemoDataSource dataSource;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diary);
 
-        Button newEntry = (Button)findViewById(R.id.button_diary);
+        dataSource = new DiaryMemoDataSource(this);
 
 
-
-
+        final Button newEntry = (Button)findViewById(R.id.button_diary);
         newEntry.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                //Wenn Button geklickt dann:
+                saveEntry();
 
-                // füge den Eintrag zur Datenbank hinzu
-                // zeige den Eintrag im ListView der MainActivity an
-                // hol dir den Ort, suche ihn auf der Karte und
-                //  markiere ihn auf der Karte mit einem Symbol
+                finish();
 
-                newDataEntry();
-                dataToList();
-                showInMap();
-
-                // spring zurück in die MainActivity:
-
-                Intent nextIntent = new Intent (DiaryActivity.this,MainActivity.class);
-                startActivity(nextIntent);
             }
         });
+
     }
 
-    private void newDataEntry(){
-        //Hinzufügen eines neuen Eintrags zur Datenbank
+
+    private DiaryMemo saveEntry(){
+
+        EditText newDate = (EditText)findViewById(R.id.edit_date_id);
+        EditText newPlace = (EditText)findViewById(R.id.edit_place_id);
+        EditText newEntry = (EditText) findViewById(R.id.edit_entry_id);
+
+        String date = newDate.getText().toString();
+        String place = newPlace.getText().toString();
+        String entry = newEntry.getText().toString();
+
+        DiaryMemo diaryMemo = dataSource.createDiaryMemo(date, place, entry);
+
+        return diaryMemo;
     }
 
-    private void dataToList(){
-        //Anzeigen des gespeicherten Eintrags in Liste in MainActivity
-    }
-
-    //erbt von MapActivity? -> selbe Methode? andere Möglichkeit?
-    //Wird es zweimal gebraucht oder reicht einmal?
-    private void showInMap(){
-        //Ort holen und in Map markieren
-        // .toString
-    }
 
 }
--- app/src/main/java/com/example/delli/myapplication/MainActivity.java
+++ app/src/main/java/com/example/delli/myapplication/MainActivity.java
@@ -3,24 +3,112 @@ package com.example.delli.myapplication;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.EditText;
+import android.view.inputmethod.InputMethodManager;
+import android.text.TextUtils;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String LOG_TAG = DiaryActivity.class.getSimpleName();
+    private DiaryMemoDataSource diaryDataSource;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button newEntry = (Button) findViewById(R.id.button_entry);
+        diaryDataSource = new DiaryMemoDataSource(this);
 
+        final Button newEntry = (Button) findViewById(R.id.button_entry);
         newEntry.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent nextIntent = new Intent (MainActivity.this,DiaryActivity.class);
+                Intent nextIntent = new Intent(MainActivity.this, DiaryActivity.class);
+                startActivity(nextIntent);
+
+            }
+        });
+
+
+        ListView listEntry = (ListView)findViewById(R.id.itemListView);
+        listEntry.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent nextIntent = new Intent(MainActivity.this, Diary2Activity.class);
+                nextIntent.putExtra("position", position);
                 startActivity(nextIntent);
             }
         });
     }
+
+
+    public void showAllListEntries(){
+
+        List<DiaryMemo> diaryMemoList = diaryDataSource.getAllDiaryMemos();
+
+        ArrayAdapter<DiaryMemo> diaryMemoArrayAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_multiple_choice, diaryMemoList);
+
+        ListView diaryMemoListView = (ListView)findViewById(R.id.itemListView);
+        diaryMemoListView.setAdapter(diaryMemoArrayAdapter);
+    }
+
+
+    @Override
+    protected void onResume(){
+
+        super.onResume();
+
+        Log.d(LOG_TAG, "Die Datenquelle wird geöffnet.");
+        diaryDataSource.open();
+
+
+
+        Log.d(LOG_TAG, "Folgende Einträge sind in der Datenbank vorhanden: ");
+        showAllListEntries();
+    }
+
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+
+        Log.d(LOG_TAG, "Die Datenquelle wird geschlossen.");
+        diaryDataSource.close();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //Inflate the menu, this adds items to the action bar if it is present
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+
+        //Actionbar clicks, automatisches handlen von Clicks (Home/Up-Button
+        //-> solange wie parent activity in AndroidManifest.xml spezifiziert
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id==R.id.action_settings){
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 }
